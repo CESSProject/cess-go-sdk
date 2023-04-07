33b1ddad6228a222b578955739b2b0c7ce5866ed
AstaFrode <32032991+AstaFrode@users.noreply.github.com>
2023-04-07 10:49:25 +0800

    update register (#5)

--- core/client/register.go
+++ core/client/register.go
@@ -1,5 +1,5 @@
 package client
 
-func (c *Cli) Register(name, multiaddr string, income string, pledge uint64) (string, error) {
-	return c.Chain.Register(name, multiaddr, income, pledge)
+func (c *Cli) Register(name string, income string, pledge uint64) (string, error) {
+	return c.Chain.Register(name, c.Multiaddr(), income, pledge)
 }
