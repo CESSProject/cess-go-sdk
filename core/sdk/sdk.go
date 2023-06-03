503a22acb481b499d845213a1ba94d81d0f4ae84
AstaFrode <frode@cess.one>
2023-06-04 00:43:14 +0800

    update sdk

--- core/sdk/sdk.go
+++ core/sdk/sdk.go
@@ -180,17 +180,5 @@ type SDK interface {
 	Sign(msg []byte) ([]byte, error)
 
 	//
-
-	//
-	GetFile(roothash, dir string) (string, error)
-	//
-	PutFile(owner []byte, segmentInfo []pattern.SegmentDataInfo, roothash, filename, bucketname string) (uint8, error)
-	//
 	ProcessingData(path string) ([]pattern.SegmentDataInfo, string, error)
-	//
-	GenerateStorageOrder(roothash string, segment []pattern.SegmentDataInfo, owner []byte, filename, buckname string) error
-	//
-	StorageData(roothash string, segment []pattern.SegmentDataInfo, minerTaskList []pattern.MinerTaskList) error
-	//
-	QueryAssignedMiner(minerTaskList []pattern.MinerTaskList) ([]string, error)
 }
