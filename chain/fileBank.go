abd384315ab23ba30dae19c1e62d295a071158c7
AstaFrode <frode@cess.one>
2023-12-20 14:12:16 +0530

    update QueryPendingReplacements (#174)

--- chain/fileBank.go
+++ chain/fileBank.go
@@ -276,48 +276,6 @@ func (c *chainClient) QueryStorageOrder(fid string) (pattern.StorageOrder, error
 	return data, nil
 }
 
-func (c *chainClient) QueryPendingReplacements(puk []byte) (types.U128, error) {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println(utils.RecoverError(err))
-		}
-	}()
-
-	var data types.U128
-
-	acc, err := types.NewAccountID(puk)
-	if err != nil {
-		return data, errors.Wrap(err, "[NewAccountID]")
-	}
-
-	owner, err := codec.Encode(*acc)
-	if err != nil {
-		return data, errors.Wrap(err, "[EncodeToBytes]")
-	}
-
-	if !c.GetChainState() {
-		return data, pattern.ERR_RPC_CONNECTION
-	}
-
-	key, err := types.CreateStorageKey(c.metadata, pattern.FILEBANK, pattern.PENDINGREPLACE, owner)
-	if err != nil {
-		err = fmt.Errorf("rpc err: [%s] [st] [%s.%s] CreateStorageKey: %v", c.GetCurrentRpcAddr(), pattern.FILEBANK, pattern.PENDINGREPLACE, err)
-		c.SetChainState(false)
-		return data, err
-	}
-
-	ok, err := c.api.RPC.State.GetStorageLatest(key, &data)
-	if err != nil {
-		err = fmt.Errorf("rpc err: [%s] [st] [%s.%s] GetStorageLatest: %v", c.GetCurrentRpcAddr(), pattern.FILEBANK, pattern.PENDINGREPLACE, err)
-		c.SetChainState(false)
-		return data, err
-	}
-	if !ok {
-		return data, pattern.ERR_RPC_EMPTY_VALUE
-	}
-	return data, nil
-}
-
 func (c *chainClient) QueryRestoralOrder(fragmentHash string) (pattern.RestoralOrderInfo, error) {
 	defer func() {
 		if err := recover(); err != nil {
