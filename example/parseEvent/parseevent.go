f836800c943fb2bb870e8d5430d41bbda8202da6
AstaFrode <frode@cess.one>
2024-01-03 16:54:15 +0800

    update event

--- example/parseEvent/parseevent.go
+++ example/parseEvent/parseevent.go
@@ -6,20 +6,23 @@ import (
 	"time"
 
 	cess "github.com/CESSProject/cess-go-sdk"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
 )
 
 // Substrate well-known mnemonic:
 //
 //	https://github.com/substrate-developer-hub/substrate-developer-hub.github.io/issues/613
-var MY_MNEMONIC = "bottom drive obey lake curtain smoke basket hold race lonely fit walk"
+//
+// var MY_MNEMONIC = "bottom drive obey lake curtain smoke basket hold race lonely fit walk"//
+var MY_MNEMONIC = "goose never chase despair dice phone penalty inside runway release cruise gain"
 
 var RPC_ADDRS = []string{
 	//devnet
 	"wss://devnet-rpc.cess.cloud/ws/",
 	//testnet
-	"wss://testnet-rpc0.cess.cloud/ws/",
-	"wss://testnet-rpc1.cess.cloud/ws/",
-	"wss://testnet-rpc2.cess.cloud/ws/",
+	// "wss://testnet-rpc0.cess.cloud/ws/",
+	// "wss://testnet-rpc1.cess.cloud/ws/",
+	// "wss://testnet-rpc2.cess.cloud/ws/",
 }
 
 func main() {
@@ -33,9 +36,17 @@ func main() {
 		panic(err)
 	}
 	// RetrieveEvent_FileBank_CalculateReport
-	bhash, err := sdk.GetSubstrateAPI().RPC.Chain.GetBlockHash(102820)
+	bhash, err := sdk.GetSubstrateAPI().RPC.Chain.GetBlockHash(72745)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(sdk.RetrieveEvent_FileBank_CalculateReport(bhash))
+	//hash, err := codec.HexDecodeString("0x5f8be5b640bf0eedd07fb886ff0f94091dae8192a4596766e878a531e697693f")
+	chash, err := types.NewHashFromHexString("0x5f8be5b640bf0eedd07fb886ff0f94091dae8192a4596766e878a531e697693f")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(bhash)
+	fmt.Println(chash)
+
+	fmt.Println(sdk.RetrieveEvent_FileBank_CalculateReport(chash))
 }
