f4f7f8fb5ea7e18d2173c66993ae416cfa9e9056
Valor Xu <121914086+jiuquxzy@users.noreply.github.com>
2024-04-15 17:43:50 +0800

    update file resumption methods and remove cacher and scheduler module (#198)
    * add cacher and scheduler module
    
    * update shared pattern
    
    * debug module
    
    * update node_selector module
    
    * update cache module
    
    * update file breakpoint resume upload function
    
    * remove cacher and scheduler
    
    * update go mod

--- chain/file.go
+++ chain/file.go
@@ -8,6 +8,7 @@
 package chain
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
@@ -17,6 +18,7 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/CESSProject/cess-go-sdk/core/pattern"
 	"github.com/CESSProject/cess-go-sdk/utils"
 	keyring "github.com/CESSProject/go-keyring"
 	"github.com/btcsuite/btcutil/base58"
@@ -247,4 +249,166 @@ func (c *chainClient) RetrieveObject(url, fid string) (io.ReadCloser, error) {
 	}
 
 	return resp.Body, nil
+}
+
+func (c *chainClient) SplitFile(fpath, chunksDir string, chunkSize int64, filling bool) (int64, int, error) {
+	fstat, err := os.Stat(fpath)
+	if err != nil {
+		return 0, 0, err
+	}
+	if fstat.IsDir() {
+		return 0, 0, errors.New("not a file")
+	}
+	if fstat.Size() == 0 {
+		return 0, 0, errors.New("empty file")
+	}
+	if fstat.Size() < chunkSize {
+		chunkSize = fstat.Size()
+	}
+	count := fstat.Size() / chunkSize
+	if fstat.Size()%chunkSize != 0 {
+		count++
+	}
+	buf := make([]byte, chunkSize)
+	f, err := os.Open(fpath)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer f.Close()
+	reader := bufio.NewReader(f)
+	size := fstat.Size()
+	for i := int64(0); i < count; i++ {
+		n, err := reader.Read(buf)
+		if err != nil {
+			return 0, 0, err
+		}
+		if n <= 0 {
+			return 0, 0, errors.New("read a empty block")
+		}
+		if n < int(chunkSize) && filling {
+			if i+1 != count {
+				return 0, 0, errors.New("read file err")
+			}
+			copy(buf[n:], make([]byte, chunkSize-int64(n)))
+			size += chunkSize - int64(n)
+			n = int(chunkSize)
+		}
+		err = utils.WriteBufToFile(buf[:n], filepath.Join(chunksDir, fmt.Sprintf("chunk-%d", i)))
+		if err != nil {
+			return 0, 0, err
+		}
+	}
+	return size, int(count), nil
+}
+
+func (c *chainClient) SplitFileWithstandardSize(fpath, chunksDir string) (int64, int, error) {
+	return c.SplitFile(fpath, chunksDir, pattern.SegmentSize, true)
+}
+
+func (c *chainClient) UploadFileChunks(url, chunksDir, bucket, fname string, chunksNum int, totalSize int64) (string, error) {
+	entries, err := os.ReadDir(chunksDir)
+	if err != nil {
+		return "", errors.Wrap(err, "upload file chunk error")
+	}
+	if len(entries) == 0 {
+		return "", errors.Wrap(errors.New("empty dir"), "upload file chunk error")
+	}
+	if len(entries) > chunksNum {
+		return "", errors.Wrap(errors.New("bad chunks number"), "upload file chunk error")
+	}
+	var res string
+	for i := chunksNum - len(entries); i < chunksNum; i++ {
+		res, err = c.UploadFileChunk(url, chunksDir, bucket, fname, chunksNum, i, totalSize)
+		if err != nil {
+			return res, errors.Wrap(err, "upload file chunks error")
+		}
+		os.Remove(filepath.Join(chunksDir, fmt.Sprintf("chunk-%d", i)))
+	}
+	return res, nil
+}
+
+func (c *chainClient) UploadFileChunk(url, chunksDir, bucket, fname string, chunksNum, chunksId int, totalSize int64) (string, error) {
+
+	file := filepath.Join(chunksDir, fmt.Sprintf("chunk-%d", chunksId))
+	fstat, err := os.Stat(file)
+	if err != nil {
+		return "", errors.Wrap(err, "upload file chunk error")
+	}
+
+	if fstat.IsDir() {
+		return "", errors.Wrap(errors.New("not a file"), "upload file chunk error")
+	}
+
+	if fstat.Size() == 0 {
+		return "", errors.New("empty file")
+	}
+
+	if !utils.CheckBucketName(bucket) {
+		return "", errors.New("invalid bucket name")
+	}
+
+	kr, _ := keyring.FromURI(c.GetURI(), keyring.NetSubstrate{})
+
+	// sign message
+	message := utils.GetRandomcode(16)
+	sig, _ := kr.Sign(kr.SigningContext([]byte(message)))
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	//
+	formFile, err := writer.CreateFormFile("file", fstat.Name())
+	if err != nil {
+		return "", err
+	}
+
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(formFile, f)
+	if err != nil {
+		return "", err
+	}
+	err = writer.Close()
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, body)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("BucketName", bucket)
+	req.Header.Set("Account", c.GetSignatureAcc())
+	req.Header.Set("Message", message)
+	req.Header.Set("Signature", base58.Encode(sig[:]))
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	req.Header.Set("FileName", fname)
+	req.Header.Set("BlockNumber", fmt.Sprint(chunksNum))
+	req.Header.Set("BlockIndex", fmt.Sprint(chunksId))
+	req.Header.Set("TotalSize", fmt.Sprint(totalSize))
+
+	client := &http.Client{}
+	client.Transport = globalTransport
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respbody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		if len(respbody) > 0 {
+			return "", errors.New(string(respbody))
+		}
+		return "", errors.New(fmt.Sprintf("upload failed, code: %d", resp.StatusCode))
+	}
+	return strings.TrimPrefix(strings.TrimSuffix(string(respbody), "\""), "\""), nil
 }
--- go.mod
+++ go.mod
@@ -9,17 +9,19 @@ require (
 	github.com/centrifuge/go-substrate-rpc-client/v4 v4.2.1
 	github.com/decred/base58 v1.0.4
 	github.com/ethereum/go-ethereum v1.10.20
+	github.com/go-ping/ping v1.1.0
 	github.com/google/uuid v1.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/klauspost/reedsolomon v1.12.0
 	github.com/mr-tron/base58 v1.2.0
+	github.com/pixelbender/go-traceroute v0.0.0-20190414152342-e631ab553a80
 	github.com/pkg/errors v0.9.1
 	github.com/samber/lo v1.39.0
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/stretchr/testify v1.8.4
 	github.com/vedhavyas/go-subkey v1.0.4
-	golang.org/x/crypto v0.17.0
-	golang.org/x/exp v0.0.0-20231226003508-02704c960a9b
+	golang.org/x/crypto v0.19.0
+	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a
 )
 
 require (
@@ -27,13 +29,14 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
-	github.com/decred/dcrd/crypto/blake256 v1.0.0 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-stack/stack v1.8.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
-	github.com/klauspost/cpuid/v2 v2.1.1 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/mimoo/StrobeGo v0.0.0-20220103164710-9a04d6ca976b // indirect
 	github.com/pierrec/xxHash v0.1.5 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -41,7 +44,9 @@ require (
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/vedhavyas/go-subkey/v2 v2.0.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
